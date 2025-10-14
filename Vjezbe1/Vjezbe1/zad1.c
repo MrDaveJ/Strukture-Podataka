19f6b149fb6a112131459b0661e36db389d6e3c2
MrDaveJ <david.jako2099@gmail.com>
2025-10-14 19:06:58 +0200

    Updateana verzija prvog zadatka

--- Vjezbe1/Vjezbe1/zad1.c
+++ Vjezbe1/Vjezbe1/zad1.c
@@ -2,53 +2,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 60
+#define MAX 60                                             // Definiranje maksimalnog broja bodova na ispitu
 
-typedef struct stud {
+
+typedef struct stud {                                                    // Stvaranje strukture 
 	char ime[20];
 	char prez[20];
 	int bod;
 	float rel;
 }stud;
 
-int count(FILE *f)  //Funkcija vraca broj linija u dokumentu
-{
-	int br = 0;
 
-	if (f == NULL)
-		return 0;
-
-	while (!feof(f)) {
-		char ch = fgetc(f);
-		if (ch == '\n')
-			br++;
-	}
-	rewind(f);
-	return br;
-}
+int count();                                            //Funkcija vraca broj linija u dokumentu tj. broj studenata
+stud* load(stud*, int);                                     // Funkcija ucitava studente iz dokumenta u strukturu
+void write(stud*, int);                                                // Funkcija ispisuje studente
 
 
 int main() {
 
-	int br, i;
-	FILE* fp = fopen("studenti.txt", "r");
+	int br;
 	stud *s;
 
-	br = count(fp);
-	printf("%d studenata\n\n", br);
+	br = count();
+	printf("\t\t%d studenata\n\n", br);
 
-	s = (stud*)malloc(sizeof(stud) * br);
+	s = (stud*)malloc(sizeof(stud) * br);                              // Dinamicko alociranje memorije
+	if (s == NULL) {
+		printf("Greska pri alociranju");
+		return -2;
+	}
 
-	for (i = 0; i < br; i++) {
-		fscanf(fp, " %s %s %d", s[i].ime, s[i].prez, &s[i].bod);
+	load(s, br);
+	write(s, br);
+
+	free(s);
+	return 0;
+}
+
+
+
+int count() {
+
+	int br = 0;
+	FILE* f = fopen("studenti.txt", "r");
+	if (f == NULL) {
+		printf("Greska pri otvaranju!");
+		return -1;
+	}
+
+	while (!feof(f)) { 
+		char ch = fgetc(f);
+		if (ch == '\n')                             // Provjera za znak '\n' koja oznacava kraj linije u txt dokumentu
+			br++;
+	}
+
+	fclose(f);
+	return br;
+}
+
+stud* load(stud* s, int br) {
+
+	int i;
+	FILE* f = fopen("studenti.txt", "r"); 
+	if (f == NULL) {
+		printf("Greska pri otvaranju!");
+		return -1;
+	}
+
+	for (i = 0; i < br; i++) {                                            // Ucitavanje studenata 
+		fscanf(f, " %s %s %d", s[i].ime, s[i].prez, &s[i].bod);
 		s[i].rel = ((float)s[i].bod / MAX) * 100;
 	}
 
+	fclose(f);
+	return s;
+}
+
+void write(stud* s, int br) {
+	int i;
 	for (i = 0; i < br; i++) {
 		printf("\t%s %s: %d bodova -> %.2f%% \n", s[i].ime, s[i].prez, s[i].bod, s[i].rel);
 	}
-
-	free(s);
-	fclose(fp);
-	return 0;
 }
